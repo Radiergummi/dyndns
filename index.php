7cc4274b0fb683528008924f2d481229011c7031
Mo <m@9dev.de>
2018-01-02 12:52:21 +0100

    Added cleaned-up index and gitignore

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+use Radiergummi\DynDns\Configuration;
+
+// enable the autoloader
+require 'vendor/autoload.php';
+
+$config = new Configuration();
+
+/* ------------------------------------------------------------------------------------
+ * Configuration options
+ *
+ * Make sure you modify these to your needs.
+ * ------------------------------------------------------------------------------------
+ */
+
+// application name as shown on the CLI
+$config->name = 'Cloudflare DynDNS';
+
+// whether to enable debugging
+$config->debug = true;
+
+// IMPORTANT: Application secret. Change this to a random string!
+// You can use the following command to generate one:
+// openssl rand -base64 32
+// $config->secret = '...';
+
+// Slim configuration options. The `displayErrorDetails` option controls display of
+// errors. Leave it like this to connect the option to the debug setting.
+$config->slimConfiguration = [
+    'settings' => [
+        'displayErrorDetails' => $config->debug
+    ]
+];
+
+/* ------------------------------------------------------------------------------------
+ * No modification required below this point
+ * ------------------------------------------------------------------------------------
+ */
+
+// define the base path to the application
+define( 'PATH', dirname( __FILE__ ) );
+
+// determine whether this is a console request
+$isCLIRequest = php_sapi_name() === 'cli';
+
+// fetch the appropriate routes
+$routes = ( $isCLIRequest
+    ? include PATH . '/src/routes/console.php'
+    : include PATH . '/src/routes/web.php'
+);
+
+// load the routes
+$config->routes = $routes;
+
+// create the kernel namespace path
+$application = 'Radiergummi\\DynDns\\' . ( $isCLIRequest
+        ? 'ConsoleKernel'
+        : 'WebKernel'
+    );
+
+//  Create the app passing the configuration
+/** @var \Radiergummi\DynDns\Kernel $app */
+$app = new $application( $config );
+
+// try to execute the application
+try {
+  $app->init();
+}
+catch ( Throwable $exception ) {
+  if ( $config->debug ) {
+    echo 'A fatal error occurred in ' . $exception->getFile() . ' on line ' . $exception->getLine();
+    echo $exception->getMessage();
+  } else {
+    echo 'Something has gone terribly wrong!';
+  }
+}
